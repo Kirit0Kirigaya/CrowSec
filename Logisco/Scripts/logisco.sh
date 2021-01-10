cd7ff72766ccda2b4cf5d423e41e59ee262d1f69
Kirito <53715699+Kirit0Kirigaya@users.noreply.github.com>
2021-01-10 19:07:29 -0300

    Update logisco.sh

--- Logisco/Scripts/logisco.sh
+++ Logisco/Scripts/logisco.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "#!/bin/bash" > /tmp/payload
-echo "echo 'flag' > /home/marcio/edival" >> /tmp/payload
+echo "echo 'CROWSEC{r00t_b1n_g1t}' > /root/root.txt" >> /tmp/payload
 
 chmod 755 /tmp/payload
 
@@ -10,4 +10,4 @@ ln -s /tmp/payload "$TF/git-x"
 sudo git "--exec-path=$TF" x
 
 rm -rf /tmp/payload
-rm -fr $TF
+rm -rf $TF
